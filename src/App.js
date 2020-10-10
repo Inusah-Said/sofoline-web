a0fd65d481a17018607f0560be7da34c38e1d913
Inusah-Said <saidinusah29@gmail.com>
2020-10-10 20:57:51 +0000

    React Router set up done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Let's build an amazon clone with Reac t JS</h1>
-		</div>
+		<Router>
+			<div className="app">
+				<Switch>
+					<Route path="/checkout">
+						<h1>Checkout</h1>
+					</Route>
+					<Route path="login">
+						<h1>Login</h1>
+					</Route>
+					<Route path="/">
+						<h1>Home</h1>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
